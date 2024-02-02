3c055591417f9443d42194c98544c4c036d103b6
HeHHeyboi <157625347+HeHHeyboi@users.noreply.github.com>
2024-02-02 15:11:32 +0700

    Update Loop.java

--- Loop.java
+++ Loop.java
@@ -1,4 +1,8 @@
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.jar.Attributes.Name;
@@ -14,6 +18,9 @@ class Customer{
 		this.id = id;
 		this.name = name;
 		this.balance = balance;
+	}
+	public Customer(String name) {
+		this.name = name;
 	}
 	public int getId() {
 		return id;
@@ -32,12 +39,29 @@ class Customer{
 class ComparatorCustomerByIdAsc implements Comparator<Customer>{
 	public ComparatorCustomerByIdAsc(){}
 	
-	@Override
-	public int compare(Customer o1,Customer o2{
-		return o1.getID()-o2.getID();
+	public int compare(Customer o1,Customer o2){
+		return o1.getId()-o2.getId();
 	}
 }
-public class Main {
+class ComparatorCustomerByName implements Comparator<Customer>{
+	
+	public ComparatorCustomerByName() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public int compare(Customer o1, Customer o2) {
+		// TODO Auto-generated method stub
+		int compV = o1.getname().compareToIgnoreCase(o2.getname());
+		if (compV == 0) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	
+}
+public class Loop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -84,8 +108,17 @@ public class Main {
 		System.out.println("\n\n Looping by foreach -method reference");
 		customers.forEach(System.out::println);// Scope resolution, used to identify function
 
-		Collection.sort(customers,new ComparatorCustomerByIdAsc());
+		Collections.sort(customers,new ComparatorCustomerByIdAsc());
 		System.out.println("== sorting customer list by ascending");
 		customers.forEach(e->System.out.println(e));
+		System.out.println("\n\nSearch by name");
+		Customer cusSearch = new Customer("John");
+		int index = Collections.binarySearch(customers,cusSearch, new ComparatorCustomerByName());
+		if(index <=0) {
+			System.out.println("Not found");
+		}
+		else {
+			System.out.println("Yes, it is "+customers.get(index));
+		}
 	}
 }
